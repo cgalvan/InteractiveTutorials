71f55996a8ca5b281d89041624807b4b19006d2c
Chris Galvan <chgalvan@amazon.com>
2022-03-29 12:36:16 -0500

    Added support for picking between multiple tutorials
    Signed-off-by: Chris Galvan <chgalvan@amazon.com>

--- Editor/Scripts/demo_tutorial.py
+++ Editor/Scripts/demo_tutorial.py
@@ -20,3 +20,16 @@ class DemoTutorial(Tutorial):
         self.add_step(TutorialStep("Select an Entity", "Next, select any Entity in the Entity Outliner", "EntityOutlinerWidgetUI"))
         self.add_step(TutorialStep("Add a component", "Use the Add Component button to add a component to the Entity", "m_addComponentButton"))
         self.add_step(TutorialStep("Cool menu bar", "This step is just to showcase highlighting an item without a direct name but by using a type pattern instead", {"type": QMenuBar}))
+
+class IntroTutorial(Tutorial):
+    def __init__(self):
+        super(IntroTutorial, self).__init__()
+
+        self.title = "Intro to the Editor"
+
+        self.add_step(TutorialStep("Entity Outliner", "Here is the Entity Outliner. This is where you can manage the entities in your level.",
+            "EntityOutlinerWidgetUI"))
+        self.add_step(TutorialStep("Asset Browser", "The Asset Browser is where you can find all your prefabs, meshes, and other assets",
+            "AzAssetBrowserWindowClass"))
+        self.add_step(TutorialStep("Console", "The Console shows you logging information and lets you run various console commands.",
+            "Console"))
